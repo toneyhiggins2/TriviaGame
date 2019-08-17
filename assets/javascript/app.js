bebffaebdd048f2642035e17978292c721a683e5
Toney Higgins <toneyhiggins2@gmail.com>
2019-08-17 18:03:38 -0400

    loading questions into js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,50 @@
+function triviaQuestions() {
+    var questOptions = [
+      // array to hold all of the questions
+      {
+        "question": "What is the name of Nas' debut album?",
+        "answers": ["The Lost Tapes", "NASIR", "Illmatic", "I Am"],
+        "correctAnswer": "answer3",
+      }, {
+        "question": "What label did Dr. Dre, Snoop Dogg and Tupac Shakur all belong to?",
+        "answers": ["Death Row", "Bad Boy", "Atlantic", "Motown"],
+        "correctAnswer": "answer1"
+      }, {
+        "question": "Who founded Bad Boy Records?",
+        "answers": ["United States","France","Germany","Brazil"],
+        "correctAnswer": "answer-2"
+      }, {
+        "question": "Name a member of Mobb Deep",
+        "answers": ["Brady Bunch","Two and A Half Men","Sopranos","The Big Bang Theory"],
+        "correctAnswer": "answer-4"
+      }, {
+        "question": "Q-Tip, Phife Dawg & Ali Shaheed Muhammad founded which group?",
+        "answers": ["Green Bay Packers","Denver Broncos","Carolina Panthers","New England Patriots"],
+        "correctAnswer": "answer-1"
+      }, {
+        "question": 'What was Christopher Wallaces stage name?',
+        "answers": ["Dale Earnhardt","Michael Schumacher","Colin McRae","Darrell Waltrip"],
+        "correctAnswer": "answer-3"
+      }, {
+        "question": "What is Jay-Z's real name?",
+        "answers": ["One","Two","Five","Seven"],
+        "correctAnswer": "answer-2"
+      }, {
+        "question": "What NY borough did the Wu-Tang Clan refer to as 'Shaolin'?",
+        "answers": ["42","80","200","153"],
+        "correctAnswer": "answer-3"
+      }, {
+        "question": "WThis artist starred in the films 'Above The Rim' and 'Juice'",
+        "answers": ["Shel Silverstein","Johnny Cash","Elvis Pressley","Elton John"],
+        "correctAnswer": "answer-1"
+      }, {
+        "question": "Will Smith starred in what TV show?",
+        "answers": ["Thomas Jefferson","Theodore Roosevelt","George Washington","Franklin Roosevelt"],
+        "correctAnswer": "answer-4"
+      }
+    ];
+    // load variables
+    amountCorrect = 0;
+  
+    return questOptions;
+  }
